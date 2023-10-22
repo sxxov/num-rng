a64dd28f2c7ca23b679d0784000eee3b68d98e2a
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-10-23 03:46:59 +0800

    Use 256 hdri

--- src/routes/lib/environment/DefaultExrHdriProvider.svelte
+++ src/routes/lib/environment/DefaultExrHdriProvider.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import ExrHdriProvider from '../../../lib/3d/environment/ExrHdriProvider.svelte';
-	import exr_hdri from '../../../assets/common/3d/hdri/dancing_hall_1k.exr?url';
+	import exr_hdri from '../../../assets/common/3d/hdri/dancing_hall_256.exr?url';
 	import { Store } from '@sxxov/ut/store';
 </script>
 
